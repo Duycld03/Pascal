b4b1bd7326f95be733d5303b177f113098bd42bb
Duy <truongduy2003@gmail.com>
2020-04-28 22:11:46 +0700

    commit

--- add_dois.pas
+++ add_dois.pas
@@ -2,7 +2,7 @@ program Add_Dots;
 uses crt;
 function add_Dots(m:longint):string;
 var text, text2:string;
-    num, num2, num3:integer;
+    num, num2, num3:extended;
 begin
     num:= m div 1000 + 1;
     num2:= 0;
